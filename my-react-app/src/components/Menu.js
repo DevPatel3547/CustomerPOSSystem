9ee3df6df0000b7439a9f48fe519d5a3cdacc4b0
Diave vrxw <dev.patel2436@gmail.com>
2023-11-06 20:12:09 -0600

    Improved cart and menu table quantity update bug

--- my-react-app/src/components/Menu.js
+++ my-react-app/src/components/Menu.js
@@ -40,108 +40,114 @@ const Menu = () => {
     setSelectedDrink(drinkName);
     setShowToppingsModal(true);
   };
+  
 
   const handleToppingChange = (drinkName, topping) => {
     setSelectedToppings(prevToppings => ({ ...prevToppings, [drinkName]: topping }));
   };
 
   const handleQuantityChange = (drinkName, increment) => {
-    setQuantities(prevQuantities => {
-      const newQuantity = increment ? prevQuantities[drinkName] + 1 : Math.max(0, prevQuantities[drinkName] - 1);
-      updateCart(drinkName, newQuantity); // Call the updateCart function whenever quantities change
-      return {
-        ...prevQuantities,
-        [drinkName]: newQuantity,
-      };
-    });
+    if (increment) {
+      // Set the selected drink and show toppings modal to add a new item
+      setSelectedDrink(drinkName);
+      setShowToppingsModal(true);
+    } else {
+      // If decrementing, just update the quantity
+      setQuantities(prevQuantities => {
+        const newQuantity = Math.max(0, prevQuantities[drinkName] - 1);
+        updateCart(drinkName, newQuantity, selectedToppings[drinkName]); // Pass the currently selected topping
+        return {
+          ...prevQuantities,
+          [drinkName]: newQuantity,
+        };
+      });
+    }
   };
   
-  const updateCart = (drinkName, newQuantity) => {
-    // Find the item in the cart
-    const existingItem = cart.find(item => item.name === drinkName);
-    if (existingItem) {
-      // If it exists and new quantity is not 0, update the quantity
-      if (newQuantity > 0) {
-        setCart(cart.map(item =>
-          item.name === drinkName ? { ...item, quantity: newQuantity } : item
-        ));
-      } else {
-        // If new quantity is 0, remove it from the cart
-        setCart(cart.filter(item => item.name !== drinkName));
-      }
-    } else if (newQuantity > 0) {
-      // If not existing and new quantity is more than 0, add a new item
+  
+  const updateCart = (drinkName, newQuantity, topping) => {
+    // Generate a unique ID for the item using drinkName and topping
+    const itemId = `${drinkName}-${topping}`;
+  
+    // Find if an item with the same unique ID already exists
+    const existingItemIndex = cart.findIndex(item => item.id === itemId);
+  
+    if (existingItemIndex > -1) {
+      // If the item already exists, update the quantity
+      const updatedCart = [...cart];
+      updatedCart[existingItemIndex] = { 
+        ...updatedCart[existingItemIndex], 
+        quantity: updatedCart[existingItemIndex].quantity + newQuantity 
+      };
+      setCart(updatedCart);
+    } else {
+      // If not, add as a new item with a unique ID
       const newItem = {
+        id: itemId, // Use the unique ID for each cart item
         name: drinkName,
-        topping: selectedToppings[drinkName] || "", // Assuming a default topping or none
+        topping: topping,
         quantity: newQuantity
       };
       setCart([...cart, newItem]);
     }
   };
   
+  
   const handleToppingDone = () => {
     if (!selectedToppings[selectedDrink]) {
       alert('Please select a topping.');
       return;
     }
-    handleQuantityChange(selectedDrink, true);
+    setShowToppingsModal(false);
     addToCart();
     setSelectedDrink(null);
   };
-
+  
 
   const addToCart = () => {
-    // Check if the item is already in the cart
-    const existingItem = cart.find(item => item.name === selectedDrink && item.topping === selectedToppings[selectedDrink]);
-  
-    if (existingItem) {
-      // If it exists, just update the quantity
-      setCart(cart.map(item =>
-        item.name === selectedDrink && item.topping === selectedToppings[selectedDrink]
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      ));
-    } else {
-      // If not, add the new item with quantity and selected topping
-      setCart([...cart, {
-        name: selectedDrink,
-        topping: selectedToppings[selectedDrink],
-        quantity: 1
-      }]);
-    }
-  
-    // Reset selections after adding to cart
-    setShowToppingsModal(false);
-  };
-
-  const handleQuantityUpdate = (itemName, increment) => {
-    // Update the cart
-    const updatedCart = cart.map(item =>
-      item.name === itemName
-        ? { ...item, quantity: increment ? item.quantity + 1 : Math.max(0, item.quantity - 1) }
-        : item
-    );
-    setCart(updatedCart);
+    // Call updateCart with the selected drink, quantity 1, and the selected topping
+    updateCart(selectedDrink, 1, selectedToppings[selectedDrink]);
     
-    // Update the quantities for the menu table
+    // Update the quantity in the menu table
     setQuantities(prevQuantities => ({
       ...prevQuantities,
-      [itemName]: increment ? prevQuantities[itemName] + 1 : Math.max(0, prevQuantities[itemName] - 1),
+      [selectedDrink]: prevQuantities[selectedDrink] + 1,
     }));
+  
+    setShowToppingsModal(false);
+    setSelectedDrink(null);
+  };
+  
+  
+
+  const handleQuantityUpdate = (itemId, increment) => {
+    // Split the item ID to get the drink name and topping
+    const [drinkName, topping] = itemId.split('-');
+    
+    // Update the cart using the unique itemId
+    const updatedCart = cart.map((item) => {
+      if (item.id === itemId) {
+        const newQuantity = increment ? item.quantity + 1 : Math.max(0, item.quantity - 1);
+        // Also, update the quantities state to reflect the change
+        setQuantities((prevQuantities) => ({
+          ...prevQuantities,
+          [drinkName]: prevQuantities[drinkName] + (increment ? 1 : -1),
+        }));
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    }).filter((item) => item.quantity > 0); // Remove the item if the quantity is 0
+    setCart(updatedCart);
+  };
+  
+  
+
+  const handleRemoveFromCart = (itemId) => {
+    // Update the cart by filtering out the item by its unique itemId
+    const updatedCart = cart.filter(cartItem => cartItem.id !== itemId);
+    setCart(updatedCart);
   };
 
-  const handleRemoveFromCart = (itemName) => {
-    // Update the cart
-    const updatedCart = cart.filter(cartItem => cartItem.name !== itemName);
-    setCart(updatedCart);
-  
-    // Reset the quantity in the menu table to 0 for the removed item
-    setQuantities(prevQuantities => ({
-      ...prevQuantities,
-      [itemName]: 0,
-    }));
-  };
   
   
 
@@ -165,7 +171,7 @@ const Menu = () => {
               <td>
                 {quantities[drink.name_of_item] > 0 ? (
                   <div>
-                    <button onClick={() => handleQuantityChange(drink.name_of_item, false)}>-</button>
+
                     {quantities[drink.name_of_item]}
                     <button onClick={() => handleQuantityChange(drink.name_of_item, true)}>+</button>
                   </div>
@@ -202,39 +208,36 @@ const Menu = () => {
       )}
 
       {showCart && (
-        <div className="modal cart-modal">
-          <div className="modal-content">
-            <h2>Cart</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th>Drink</th>
-                  <th>Topping</th>
-                  <th>Quantity</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.map(item => (
-                  <tr key={item.name}>
-                    <td>{item.name}</td>
-                    <td>{item.topping}</td>
-                    <td>
-                      <button onClick={() => handleQuantityUpdate(item.name, false)}>-</button>
-                      {item.quantity}
-                      <button onClick={() => handleQuantityUpdate(item.name, true)}>+</button>
-                    </td>
-                    <td>
-                      <button onClick={() => handleRemoveFromCart(item.name) }>Remove</button>
-                    </td>
+          <div className="modal cart-modal">
+            <div className="modal-content">
+              <h2>Cart</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Drink</th>
+                    <th>Topping</th>
+                    <th>Quantity</th>
+                    {/* Last column for actions (Remove button) is now removed */}
                   </tr>
-                ))}
-              </tbody>
-            </table>
-            <button onClick={() => setShowCart(false)}>Close</button>
+                </thead>
+                <tbody>
+                  {cart.map((item) => (
+                    <tr key={item.id}>
+                      <td>{item.name}</td>
+                      <td>{item.topping}</td>
+                      <td>
+                        <button onClick={() => handleQuantityUpdate(item.id, false)}>-</button>
+                        {item.quantity}
+                        <button onClick={() => handleQuantityUpdate(item.id, true)}>+</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <button onClick={() => setShowCart(false)}>Close</button>
+            </div>
           </div>
-        </div>
-      )}
+       )}
     </div>
   );
 };
