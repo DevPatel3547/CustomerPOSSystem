825b35a8d948de2116fa4d2de46656314147cfec
Diave vrxw <dev.patel2436@gmail.com>
2023-12-07 11:13:40 -0600

    Reverted login page due to accessibility issues

--- my-react-app/src/components/managerLogin.js
+++ my-react-app/src/components/managerLogin.js
@@ -1,19 +1,14 @@
-// src/components/managerlogin.js
+// src/components/login.js
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
-import { useState } from 'react';
 import './login.css';
 
 
 const Login = () => {
     let navigate = useNavigate();
-    const [userId, setUserId] = useState('');
-    const [password, setPassword] = useState('');
 
-    const allowedEmails = ["avirala16@tamu.edu", "devrpatel04@tamu.edu", "ishaan2947@tamu.edu",	"jack.payne@tamu.edu", "shreyask25@tamu.edu"]; // List of allowed emails
-
-
+    const allowedEmails = ["avirala16@tamu.edu", "devrpatel04@tamu.edu", "ishaan2947@tamu.edu", "jack.payne@tamu.edu", "shreyask25@tamu.edu"]; // List of allowed emails
 
     const handleLoginSuccess = (credentialResponse) => {
         console.log(credentialResponse);
@@ -25,7 +20,7 @@ const Login = () => {
             const userEmail = decodedResponse.email;
     
             if (allowedEmails.includes(userEmail)) {
-                navigate('/cashier');
+                navigate('/manager');
             } else {
                 alert('Access Denied: Please enter a valid email');
                 
@@ -34,45 +29,30 @@ const Login = () => {
             console.error('Error decoding token:', error);
         }
     };
-    
 
-    const handleLoginFailure = () => {
-        alert('Access Denied: Please enter a valid email');
-    };
+const handleLoginFailure = () => {
+     alert('Access Denied: Please enter a valid email');
+};
   
-
-    const handleManualLogin = (e) => {
-        e.preventDefault();
-        if (userId === 'manager' && password === 'managing') {
-            navigate('/manager');
-        } else {
-            alert('Invalid credentials');
-        }
-    };
-
     return (
-        <div className="login">
-            <h1 className="title" style={{color: 'white'}}>Welcome</h1>
-            <div className="backButton">
-                <button onClick={() => navigate('/')}>Back</button>
-            </div>
-
-            <div className="googleAuth">
-                <p>Please Sign in with Google</p>
-                <GoogleOAuthProvider clientId="241129987927-u7tu85kmdrs0ref1delh9gal1p3b75s5.apps.googleusercontent.com">
-                    <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginFailure} />
-                </GoogleOAuthProvider>
-            </div>
-
-            <div className="manualAuth">
-                <p>or manually login as cashier</p>
-                <form onSubmit={handleManualLogin}>
-                    <input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="User ID" />
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        </div>
+      <div className="login">
+        
+          <h1 className="title" style={{color: 'white'}}>Welcome</h1>
+          <div className = "backButton">
+      <button onClick={() => navigate('/')}>Back</button>
+      </div>
+      
+          <div className="googleAuth">
+            <p>Please Sign in</p>
+          <GoogleOAuthProvider clientId="241129987927-u7tu85kmdrs0ref1delh9gal1p3b75s5.apps.googleusercontent.com">
+          <GoogleLogin
+             onSuccess={handleLoginSuccess}
+             onError={handleLoginFailure}
+            />
+    </GoogleOAuthProvider>
+          </div>
+          
+      </div>
     );
   };
   
